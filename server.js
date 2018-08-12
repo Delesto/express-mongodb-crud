c6d200d96e5aafd39e690bacf1ef0e5c6b4effd2
Cyrus <teyanesst@gmail.com>
2018-08-12 23:40:33 +0300

    added signIn / signUp routes and views

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const config = require('./config');
 const app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.use(express.static('./public'))
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 //Routes
 const index = require('./routes/index');
+const signin = require('./routes/signin');
+const signup = require('./routes/signup');
 app.use('/', index);
+app.use('/signin', signin);
+app.use('/signup', signup);
 
 app.listen(config.app.port, config.app.url, () => {
     console.log(`App running on port ${config.app.port}`);
